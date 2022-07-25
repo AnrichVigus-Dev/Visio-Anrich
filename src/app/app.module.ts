609effb9b2683b6ac3fb9110f43e1251227f5728
AnrichVigus <83713185+AnrichVigus-Dev@users.noreply.github.com>
2022-07-25 07:08:09 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { NavComponent } from './nav/nav.component';
+import { DescriptionComponent } from './description/description.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { FundsComponent } from './funds/funds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    NavComponent,
+    DescriptionComponent,
+    ButtonsComponent,
+    FundsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/slider/slider.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var name: any;
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+
+  constructor() {
+
+   }
+
+  ngOnInit()  {
+    
+
+
+  }
+
+}
+
+
